51a83988cad6398e74942de3be13c90845babc76
Andy Truong <thehongtt@gmail.com>
2013-11-17 11:53:40 +0700

    Make it able to download dependencies for specific module.

--- lib/Drush/Command/AtRequire.php
+++ lib/Drush/Command/AtRequire.php
@@ -5,17 +5,33 @@ namespace Drupal\at_require\Drush\Command;
 use \Drupal\at_require\Drush\Command\AtRequire\DependenciesFetcher;
 
 class AtRequire {
+  private $module;
+
+  public function __construct($module = 'all') {
+    $this->module = $module;
+  }
+
   /**
    * Get supported modules.
    */
   private function getModules() {
     $modules = array();
-    foreach (at_modules('at_base') as $module) {
-      $file = DRUPAL_ROOT . '/' . drupal_get_path('module', $module) . '/config/at_require.yml';
-      if (file_exists($file)) {
-        $modules[] = $module;
+
+    if ($this->module === 'all') {
+      foreach (at_modules('at_base') as $module) {
+        $file = DRUPAL_ROOT . '/' . drupal_get_path('module', $module) . '/config/at_require.yml';
+        if (file_exists($file)) {
+          $modules[] = $module;
+        }
       }
     }
+    else {
+      if (!module_exists($this->module)) {
+        throw new \Exception('Invalid module: ' . $this->module);
+      }
+      $modules[] = $module;
+    }
+
     return $modules;
   }
 
